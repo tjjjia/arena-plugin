7a48afa68902ec9064385d64c8b551fe28ad569f
blank <14137521+tjjjia@users.noreply.github.com>
2023-11-05 21:32:51 +0100

    ✨ 0.1.0

--- main.ts
+++ main.ts
@@ -1,85 +1,216 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, requestUrl, RequestUrlParam, Setting, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ArenaPluginSettings {
+	storeInfo: false;
+	arenaAccessToken: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ArenaPluginSettings = {
+	storeInfo: false,
+	arenaAccessToken: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface EmbedInfo {
+	title: string;
+	arenaUrl: string;
+	arenaClass: string,
+	userUsername: string;
+	userUrl: string;
+	imageSquareUrl: string;
+	imageThumbUrl: string;
+	sourceUrl: string;
+	contentHTML: string;
+	embedFound: boolean;
+	networkError: boolean;
+	message: string;
+}
+
+export default class ArenaPlugin extends Plugin {
+	settings: ArenaPluginSettings;
+
+	getAllLeaves() {
+		const ret = [] as WorkspaceLeaf[];
+		this.app.workspace.iterateAllLeaves(leaf => { ret.push(leaf) })
+		return ret;
+	}
+	
+	createDummyBlock(el: HTMLElement) {
+		const container = el.createDiv();
+		container.addClass('dummy-container');
+	}
+
+	removeDummyBlock(el: HTMLElement) {
+		const dummy = el.querySelector('.dummy-container');
+		if(dummy){
+			el.removeChild(dummy);
+		}
+	}
+
+	async getEmbedInfo(url: string): Promise<EmbedInfo> {
+		const info: EmbedInfo = {
+			title: '',
+			arenaUrl: '',
+			arenaClass: '',
+			userUsername: '',
+			userUrl: '',
+			imageSquareUrl: '',
+			imageThumbUrl: '',
+			sourceUrl: '',
+			contentHTML: '',
+			embedFound: false,
+			networkError: false,
+			message: '',
+		};
+		
+		const pattern = /(?:https?:\/\/)?(?:www\.)?are\.na\/(block)\/(\d+)\/?/i;
+		const match = url.match(pattern);
+
+		let className, arenaID;
+		if (match) {
+			className = match[1];
+			arenaID = match[2];
+		} else {
+			console.log("URL doesn't match the pattern.");
+			info.message = `URL doesn't match the pattern.`;
+			return info;
+		}
+		
+		let reqUrl;
+		switch (className) {
+			case "block":
+				info.arenaUrl = match[0];
+				reqUrl = `https://api.are.na/v2/blocks/${arenaID}`;
+				break;
+			default:
+				console.log("URL doesn't match the pattern.");
+				info.message = `URL doesn't match the pattern.`;
+				return info;
+				break;
+		}
+
+		try {
+			const headers = {
+			};
+			const reqParam: RequestUrlParam = {
+				url: reqUrl,
+				throw: false,
+				headers
+			};
+			const res = await requestUrl(reqParam);
+			if (res.status === 200){
+				// console.log( res.json );
+
+				info.title =  res.json.title,
+				// info.arenaUrl =  '',
+				info.arenaClass =  res.json.class.toLowerCase(),
+				info.userUsername =  res.json.user.username;
+				info.userUrl = `https://www.are.na/${res.json.user.slug}`;
+				info.imageSquareUrl = (res.json.image) ? res.json.image.square.url : '';
+				info.imageThumbUrl = (res.json.image) ? res.json.image.thumb.url : '';
+				info.sourceUrl = (res.json.source) ? res.json.source.url : '';
+				info.contentHTML = res.json.content_html;
+				info.embedFound =  true;
+				info.networkError =  false;
+				info.message =  'Success!';
+			} else {
+				console.log( res );
+				const text = JSON.parse(res.text);
+				info.message = `${text.message} (${text.code}), ${text.description}`;
+			}
+			
+		} catch (error) {
+			console.error(error);
+			// Network error
+			info.networkError = true;
+			info.message = `Network error.`;
+		}
+		
+		return info;
+	}
+
+	createEmbed(el: HTMLElement, info: EmbedInfo) {
+		const container = el.createEl('a', {href: info.arenaUrl} );
+		container.addClass('arena--block');
+
+		const boxElement = container.createDiv();
+		boxElement.addClass('arena--block--inner')
+		boxElement.setAttribute('data-class', info.arenaClass );
+
+		switch (info.arenaClass) {
+			case "image":
+			case "link":
+				const imgElement = boxElement.createEl('img', { attr: { 'src': info.imageSquareUrl } });
+				break;
+			case "text":
+				const textContainer = boxElement.createDiv()
+				textContainer.innerHTML = info.contentHTML;
+				break
+			case "attachment":
+			case "channel":
+				break;
+			default:
+				break;
+		}
+
+		container.addClass('arena--block');
+		const textElement = container.createDiv({text: info.title, title: info.title})
+			.addClass('arena--block--caption');
+
+		const buttonElement = boxElement.createEl('a', {href: info.sourceUrl, text:"Source" } )
+			.addClass('arena--block--button');
+
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerMarkdownCodeBlockProcessor('arena', async (source, el, ctx) => {
+			// console.log( source, el, ctx );
+			this.createDummyBlock(el);
+			el.setAttribute('data-status','busy');
+
+			let sourceLines = source.trim().split('\n'); // array of urls from lines
+			sourceLines = sourceLines.filter(Boolean); // drop empty lines
+
+			sourceLines.forEach( async (url) =>{
+				let info: EmbedInfo;
+
+				info = await this.getEmbedInfo(url.toLowerCase());
+				// console.log(info)
+
+				// success
+				if (info.embedFound){
+					this.removeDummyBlock(el);
+					this.createEmbed(el, info);
+					el.setAttribute('data-status','ready');
+
+				// something wrong
+				} else {
+					this.removeDummyBlock(el);
+					el.setAttribute('data-status','error');
+					const aElement = el.createEl('a', { text: url, href: url });
+					const spanElement = el.createEl('span', { text: info.message });
+					spanElement.addClass('arena--error--message');
+
+				}
+			})
 		});
 
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			// console.log('✨ Click detected', evt);
+			// console.log('✨ Click detected');
+		// });
+
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -91,26 +222,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: ArenaPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ArenaPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +236,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Are.na Plugin')
+			.setDesc('Test')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.arenaAccessToken)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.arenaAccessToken = value;
 					await this.plugin.saveSettings();
 				}));
 	}
